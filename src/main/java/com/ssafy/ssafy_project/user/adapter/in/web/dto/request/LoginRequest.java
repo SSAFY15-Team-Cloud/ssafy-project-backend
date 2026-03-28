6bda640887d2be2cac9a98c9fb2b8520f79c8e5f
csk6314 <37896060+csk6314@users.noreply.github.com>
2026-03-28 19:26:40 +0900

    feature: AuthController 구현

--- src/main/java/com/ssafy/ssafy_project/user/adapter/in/web/dto/request/LoginRequest.java
+++ src/main/java/com/ssafy/ssafy_project/user/adapter/in/web/dto/request/LoginRequest.java
@@ -1,12 +1,7 @@
 package com.ssafy.ssafy_project.user.adapter.in.web.dto.request;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-public class LoginRequest {
-
-    private String email;
-    private String password;
-}
+public record LoginRequest(
+        String email,
+        String password
+) {}
